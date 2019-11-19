79be04c98624d7027ece72d92601cbb1d011daa4
Jared Brown <jared@Jareds-MBP.lan1>
2019-11-18 23:47:51 -0800

    added title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../src/components/nav-bar";
+import Title from "../src/components/title";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Title />
       <NavBar />
       <p>
         Read through the instructions in the README.md file to build your NASA
--- src/components/title.js
+++ src/components/title.js
@@ -1,1 +1,11 @@
 import React from "react";
+
+const Title = props => {
+  return (
+    <div>
+      <h1>Nasa Photo Of The Day!</h1>
+    </div>
+  );
+};
+
+export default Title;
