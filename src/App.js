e48a27f1ed14676c34a3e573560a362962917695
Jared Brown <jared@Jareds-MBP.lan1>
2019-11-18 23:31:53 -0800

    got a nav bar showing with jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import NavBar from "../src/components/nav-bar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
---
+++ src/components/nav-bar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = props => {
+  return (
+    <div>
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Space</a>
+      <a href="#">Black-hole</a>
+    </div>
+  );
+};
+
+export default NavBar;
